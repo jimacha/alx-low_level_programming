b9366c7b2fb1635bc788ac2d37b951e4d708390d
jimacha <machariajimmy68@gmail.com>
2022-12-09 04:40:05 -0800

    23

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studion.h>
+#include <stdio.h>
 /**
  * main - Entry
  * Return: Always 0
  */
+
 int main(void)
 {
-	puts("\" "Programming is like building a multilingual puzzle");
-	return (0)
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
