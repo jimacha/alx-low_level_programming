872471eb4cd8dea978e71a595bb64b5e8be27be5
jimacha <machariajimmy68@gmail.com>
2022-12-12 05:24:51 -0800

    4-print_alphabt.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always (zero)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
