c25f9e77c4915e90e2232324d181d02d0ce48f2e
jimacha <machariajimmy68@gmail.com>
2022-12-12 08:34:31 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 	int i;
 	int j;
 
-	for (j = 0; j <= 9; j++)
+	for (j = '0'; j <= '9'; j++)
 	{
-		for (i = 0; i <= 9; i++)
+		for (i = '0'; i <= '9'; i++)
 		{
 			if (!((i == j) || (i < j)))
 			{
 				putchar(j);
 				putchar(i);
-				if (!((i == 9) && (j == 8)))
+				if (!((i == '9') && (j == '8')))
 				{
 					putchar(',');
 					putchar(' ');
