ba324a9172427148cca772850f89085ba7518f98
jimacha <machariajimmy68@gmail.com>
2022-12-15 04:49:34 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+
+	while (i < 10)
+	{
+		j = 'a'
+		while (j <= 'z';)
+		{
+			_putchar(j);
+			j++
+		}
+		_putchar("\n");
+	}
+}
