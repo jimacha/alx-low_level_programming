3091468ca8c0ec7be8803173f357f60f66b2ed25
jimacha <machariajimmy68@gmail.com>
2022-12-15 12:42:36 -0800

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Terst function for positive or negative
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i =0;
+	positive_or_negative(i);
+
+	return (0);
+}
