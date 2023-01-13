4f0e6139a4ccccfcbb5da7b5b4e6178de1431c93
jimacha <machariajimmy68@gmail.com>
2023-01-13 14:39:23 -0800

    clean mand

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = NULL;
-	
-	ptr = (void *)malloc(b);
+	void *ptr;
 
+	ptr = malloc(b);
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	return (ptr);
+	return (ptr)
 }
