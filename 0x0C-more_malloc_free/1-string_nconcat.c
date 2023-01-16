e52af7894c3b2358ddd574ba177b6459e0059345
jimacha <machariajimmy68@gmail.com>
2023-01-16 00:35:19 -0800

    corrects

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,25 +1,39 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ * Return: pointer to the resulting string
  */
-char *_strdup(char *str)
-{
-	char *aaa;
-	int i, r = 0;
 
-	if (str == NULL)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	unsigned int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+	char *result = malloc(len1 + n + 1);
+
+	if (result == NULL)
+	{
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	aaa = malloc(sizeof(char) * (i + 1));
-	if (aaa == NULL)
-		return (NULL);
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-	return (aaa);
+	}
+	strcpy(result, s1);
+	strncat(result, s2, n);
+
+	return (result);
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -40,19 +40,19 @@ int _strlen(char *s)
 void errors(void)
 {
 	printf("Error\n");
-	exit(98);		
+	exit(98)
 }
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * 		
- * 		Return: always 0 (Success)
+ * Return: always 0 (Success)
  */
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
