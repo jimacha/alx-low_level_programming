80ffbfa9ababd0a5c56c6a154c529d159b0218f4
jimacha <machariajimmy68@gmail.com>
2023-01-15 23:41:22 -0800

    line 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,25 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-
-char *string_nconcat(char *s1, char *s2, unsigned int n) 
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
 {
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
-	if (n >= len2)
-	{
-		n = len2;
-	}
-	char *result = malloc(len1 + n + 1);
-	if (result == NULL) 
-	{
+	char *aaa;
+	int i, r = 0;
+
+	if (str == NULL)
 		return (NULL);
-	}
-	strcpy(result, s1);
-	strncat(result, s2, n);
-	
-	return (result);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	aaa = malloc(sizeof(char) * (i + 1));
+	if (aaa == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+	return (aaa);
 }
