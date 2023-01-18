87a6005d51dd2b5c7b5ddc6f437537ae3884904e
jimacha <machariajimmy68@gmail.com>
2023-01-17 16:47:20 -0800

    mm

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-
+#include <unistd.h>
+#include <string.h>
 /**
  * main - prints the name of the file
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	char fileName[] = _FILE_;
+	int len = strlen(fileName);
+
+	write(STDOUT_FILENO, fileName + 1, len - 2);
+
+	char newLine[] = "\n";
+
+	write(STDOUT_FILENO, newLine, 1);
 	return (0);
 }
