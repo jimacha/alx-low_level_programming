29bf88d1fb626189ba4290c27ba27fd034b31639
jimacha <machariajimmy68@gmail.com>
2023-02-02 13:41:33 -0800

    headerfile

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,25 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/**
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- * for Holberton project
+ * for alx project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+	 int n;
+	  struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
